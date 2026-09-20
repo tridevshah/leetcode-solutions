f4a2261b701c71998e67f4a43f061977a40e58a7
Tridev Shah <tridevshah4@gmail.com>
2026-09-20 12:40:37 +0530

    Time: 357 ms (80.32%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,22 +1,12 @@
 # Write your MySQL query statement below
 SELECT
-    id,
+    CASE
+        WHEN id % 2 = 1 AND id <> (SELECT MAX(id) FROM Seat)
+            THEN id + 1
+        WHEN id % 2 = 0
+            THEN id - 1
+        ELSE id
+    END AS id,
     student
-FROM (
-    SELECT
-        CASE
-            WHEN id % 2 = 1 AND next_id IS NOT NULL THEN next_id
-            WHEN id % 2 = 0 THEN previous_id
-            ELSE id
-        END AS id,
-        student
-    FROM (
-        SELECT
-            id,
-            student,
-            LAG(id) OVER (ORDER BY id) AS previous_id,
-            LEAD(id) OVER (ORDER BY id) AS next_id
-        FROM Seat
-    ) t
-) x
-ORDER BY id ASC;
+FROM Seat
+ORDER BY id
