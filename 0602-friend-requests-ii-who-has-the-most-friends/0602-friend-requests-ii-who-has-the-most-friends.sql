453ae64f39b6796f1663d0b2d83606f25a8ab179
Tridev Shah <tridevshah4@gmail.com>
2026-09-20 12:10:10 +0530

    Time: 830 ms (5.02%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT 
+id,
+COUNT(id) AS num
+FROM (
+    SELECT 
+    requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT 
+    accepter_id AS id
+    FROM RequestAccepted
+    )t
+GROUP BY id
+ORDER BY COUNT(id) DESC
+LIMIT 1
