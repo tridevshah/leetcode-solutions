ae53a99bdcc8b37b5e07b13e0b22a3acd807d721
Tridev Shah <tridevshah4@gmail.com>
2026-09-22 15:55:39 +0530

    Time: 3271 ms (5.01%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,34 +1,24 @@
 # Write your MySQL query statement below
-WITH user_rank AS (
-    SELECT
-        u.name,
-        ROW_NUMBER() OVER (
-            ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
-        ) AS rn
-    FROM Users AS u
-    JOIN MovieRating AS mr
+(
+    SELECT u.name AS results
+    FROM Users u
+    JOIN MovieRating mr
         ON u.user_id = mr.user_id
     GROUP BY u.user_id, u.name
-),
+    ORDER BY COUNT(mr.movie_id) DESC, u.name ASC
+    LIMIT 1
+)
 
-movie_rank AS (
-    SELECT
-        m.title,
-        ROW_NUMBER() OVER (
-            ORDER BY AVG(mr.rating) DESC, m.title ASC
-        ) AS rn
-    FROM Movies AS m
-    JOIN MovieRating AS mr
+UNION ALL
+
+(
+    SELECT m.title AS results
+    FROM Movies m
+    JOIN MovieRating mr
         ON m.movie_id = mr.movie_id
     WHERE mr.created_at >= '2020-02-01'
       AND mr.created_at < '2020-03-01'
     GROUP BY m.movie_id, m.title
-)
-
-SELECT name AS results
-FROM user_rank
-WHERE rn = 1
-UNION ALL
-SELECT title AS results
-FROM movie_rank
-WHERE rn = 1;
+    ORDER BY AVG(mr.rating) DESC, m.title ASC
+    LIMIT 1
+);
